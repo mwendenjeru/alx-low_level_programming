6cf74ac576dd89a81dd65fb120be04b108516fc5
mwendenjeru <mwendeafrica@gmail.com>
2023-06-19 07:50:12 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,20 +3,16 @@
  * main - entry point
  * Return: always 0
  * Program prints the alphabet in lowercase
- * and then in lower case
+ * and then in uppercase
  */
 int main(void)
 {
-	char lowercase = 'a';
-	char uppercase = 'A';
-	int i;
-
-	for (i = 0; i < 26; i++)
-		putchar(lowercase);
-	lowercase++;
-	for (i = 0; i < 26; i++)
-		putchar(uppercase);
-	uppercase++;
+	int letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 	putchar('\n');
 	return (0);
 }
