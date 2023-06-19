01b491cc03a6a24e70e18d04c5e0558026e40c33
mwendenjeru <mwendeafrica@gmail.com>
2023-06-19 12:15:47 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,20 @@ int main(void)
 {
 	int num1;
 	int num2;
-	int num3;
-	int num4;
-
-	for (num1 = 0; num1 <= 9; num1++)
+	
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (num3 = 0; num3 <= 9; num3++)
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 != 98 || num2 != 99)
 			{
-				for (num4 = num3 + 1; num4 <= 9; num4++)
-				{
-					putchar((num1 % 10) + '0');
-					putchar((num2 % 10) + '0');
-					putchar(' ');
-					putchar((num3 % 10) + '0');
-					putchar((num4 % 10) + '0');
-					if (num1 != 9 || num2 != 8 || num3 != 9 || num4 != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
