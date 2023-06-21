773e35a3febcd13693e84158e4e70e83581c71f6
mwendenjeru <mwendeafrica@gmail.com>
2023-06-20 17:55:30 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - Prints the sign of a number.
  * @n: The number to check.
@@ -7,20 +8,21 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return(0);
-    }
-    else
-    {
-        _putchar('-');
-        return (-1);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
+
 
