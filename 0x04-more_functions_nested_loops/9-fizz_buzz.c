5338eca475dbca82047c6f4081fc8a79eb2c4adf
mwendenjeru <mwendeafrica@gmail.com>
2023-06-22 09:01:06 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,70 +1,22 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main - entry point
- * Return - always 0
+ * Return: always 0
  */
-int _putchar(char c);
-void fizz_buzz(void);
 int main(void)
 {
-	fizz_buzz();
-	return (0);
-}
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-void fizz_buzz(void)
-{
 	int i;
-	
+
 	for (i = 1; i <= 100; i++) {
-		if (i % 3 == 0 && i % 5 == 0) {
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar(' ');
-		}
-		else if (i % 3 == 0) {
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar(' ');
-		}
-		else if (i % 5 == 0) {
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-			_putchar(' ');
-		}
-		else {
-			int num = i;
-			int digits = 0;
-
-			while (num != 0) {
-				num /= 10;
-				digits++;
-			}
-			while (digits > 0) {
-				int divisor = 1;
-				int j;
-
-				for (j = 1; j < digits; j++)
-					divisor *= 10;
-				num = i / divisor;
-				_putchar(num + '0');
-				i %= divisor;
-				digits--;
-			}
-			_putchar(' ');
-		}
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
 	}
-	_putchar('\n');
+	printf("\n");
+	return (0);
 }
