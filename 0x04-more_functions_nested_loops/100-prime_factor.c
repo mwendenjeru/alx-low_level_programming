9190ba4e959354f0fa82fdd24f63bbead6286666
mwendenjeru <mwendeafrica@gmail.com>
2023-06-25 02:50:25 -0700

    The problem of distinguishing prime numbers from composite numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,33 +9,29 @@
  * Return: The largest prime factor of the number
  */
 
-long largest_prime_factor(long n)
+int main(void)
 {
-	long largest_factor = -1;
-	long i;
+	long int n;
+	long int i;
+	long int largest_factor;
 
+	n = 612852475143;
+	largest_factor = -1;
 	while (n % 2 == 0)
 	{
 		largest_factor = 2;
 		n /= 2;
 	}
-	for (i = 3; i <= sqrt(n); i += 2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
-			largest_factor = i;
-			n /= i;
+		       largest_factor = i;
+			n = n / i;
 		}
 	}
 	if (n > 2)
 		largest_factor = n;
-	return largest_factor;
-}
-int main(void)
-{
-	long number = 612852475143;
-	long largest_factor = largest_prime_factor(number);
-	
 	printf("%ld\n", largest_factor);
 	return (0);
 }
