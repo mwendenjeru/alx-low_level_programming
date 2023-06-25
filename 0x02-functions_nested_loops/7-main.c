c681d8ef70a2ad0d4b36e9115847002d80c2ec90
mwendenjeru <mwendeafrica@gmail.com>
2023-06-25 06:24:51 -0700

    There are only 3 colors, 10 digits, and 7 notes

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -2,24 +2,17 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = print_last_digit(98);
-    _putchar('0' + r);
-    _putchar('\n');
-
-    r = print_last_digit(0);
-    _putchar('0' + r);
-    _putchar('\n');
-
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-
-    return (0);
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
 }
-
