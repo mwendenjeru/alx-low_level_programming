9a70dad9d0ddc42c8bb20ef5b733b6e4192b221d
mwendenjeru <mwendeafrica@gmail.com>
2023-06-26 16:33:05 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,44 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#define PASSWORD_LENGTH 8
-
 /**
- * generateRandomChar - Generates a random character
- * Return: The generated random character
- */
-char generateRandomChar(void)
-{
-	int randomAscii = rand() % 94 + 33;
-
-	return ((char)randomAscii);
-}
-/**
- * generateRandomPassword - Generates a random password
- * @password: The buffer to store the generated password
- */
-void generateRandomPassword(char *password)
-{
-
-	int i;
-
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-	{
-		password[i] = generateRandomChar();
-	}
-	password[PASSWORD_LENGTH] = '\0';
-}
-/**
- * main - Entry point of the program
- * Return: Always 0 (Success)
+ * main - entry point
+ * Return: always 0
  */
 int main(void)
 {
-	char password[PASSWORD_LENGTH + 1];
+	int pw[100];
+	int i, sum, n;
 
+	sum = 0;
 	srand(time(NULL));
-	generateRandomPassword(password);
-	printf("%s\n", password);
+	for (i = 0; i < 100; i++)
+	{
+		pw[i] = rand() % 78;
+		sum += (pw[i] + '0');
+		putchar(pw[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 	return (0);
 }
+
