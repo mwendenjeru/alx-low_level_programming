fa7a12b048bb447373459017fed4c3e54737f810
mwendenjeru <mwendeafrica@gmail.com>
2023-06-29 19:40:12 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ char *rot13(char *str);
 void _putchar(char c);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
 
