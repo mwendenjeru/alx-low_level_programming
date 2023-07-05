174348b09cbbdd5350e24e908e0be5e8c619309f
mwendenjeru <mwendeafrica@gmail.com>
2023-07-05 05:58:20 -0700

    Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    _putchar('\n');
+    return (0);
+}
