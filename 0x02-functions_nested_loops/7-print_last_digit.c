6aabc0ac9d30d44b9a63480b4fbd07f19c78fd67
mwendenjeru <mwendeafrica@gmail.com>
2023-07-07 13:57:46 -0700

    There are only 3 colors, 10 digits, and 7 notes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,16 @@
 
 /**
  * print_last_digit - Prints the last digit of a number
- * @n: The number
+ * @number: The number to extract the last digit from
  * Return: The value of the last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int number)
 {
 	int last_digit;
 
-	if (n < 0)
-		n = -n;
-	last_digit = n % 10;
-	last_digit = -last_digit;
+	if (number < 0)
+		number = -number;
+	last_digit = number % 10;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int n);
+int print_last_digit(int number);
 void jack_bauer(void);
 void times_table(void);
 int add(int num1, int num2);
