05fad5aa393830d8a9a37879ba6666f5c1e73ccf
mwendenjeru <mwendeafrica@gmail.com>
2023-07-11 08:52:00 -0700

    I will show you how great I am

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,4 +8,8 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int count_words(char *str);
+int word_length(char *str);
+void free_words(char **words);
+
 #endif /* MAIN_H */
