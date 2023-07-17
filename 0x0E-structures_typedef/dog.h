ba390645015d40d7c4eefd4510f0153e0dec6968
mwendenjeru <mwendeafrica@gmail.com>
2023-07-17 10:33:15 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Represents a dog with its attributes
+ * @name: Pointer to a character representing the name of the dog
+ * @age: Float representing the age of the dog
+ * @owner: Pointer to a character representing the owner of the dog
+ */
 
 struct dog
 {
