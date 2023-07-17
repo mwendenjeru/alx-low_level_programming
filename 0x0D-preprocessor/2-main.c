3671d1f83255e6605449c752ab28196b3e7a5924
mwendenjeru <mwendeafrica@gmail.com>
2023-07-17 08:20:41 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
+#ifndef FILENAME
+#define FILENAME __FILE__
+#endif
+
 /**
  * main - entry point
  * Return: always 0
  */
 int main(void)
 {
-	printf("Compiled from file: %s\n", __FILE__);
+	printf("Compiled from file: %s\n", FILENAME);
 	return (0);
 }
